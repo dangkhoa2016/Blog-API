e01f1324b5f6cc48003dabbb6de89d346af08910
Romulo Esquivel <52230585+romuloesquivel1@users.noreply.github.com>
2023-11-10 07:31:33 +0000

    improve db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,38 @@
-puts 'creating account ....'
+puts 'creating random user account ....'
 
-# User
+# Users
 10.times do
   pwd = Faker::Internet.password
-  hash = { first_name: Faker::Name.unique.name, email: Faker::Internet.email }
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  email = Faker::Internet.email(name: "#{first_name} #{last_name}", separators: '.')
+  hash = { first_name: first_name, last_name: last_name, email: email }
   hash.merge!({ password: pwd })
   user = User.new(hash)
   user.password_confirmation = pwd
   user.save
 end
 
-# POSTS
-@user = User.first
-puts 'creating posts ....'
-50.times do
-  title = Faker::Lorem.sentence
-  content =  Faker::Lorem.sentence(word_count: 30)
-  hash = { title: title, content: content }
-  @user.posts.new(hash).save
+# Posts
+puts 'creating random posts for each user ....'
+User.all.each do |user|
+  rand(3..30).times do
+    title = Faker::Lorem.sentence
+    content =  Faker::Lorem.sentence(word_count: 30)
+    hash = { title: title, content: content }
+    user.posts.new(hash).save
+  end
 end
 
-# comments
-puts 'creating comment ....'
-
-post = Post.first
-100.times do
-  content =  Faker::Lorem.sentence(word_count: 30)
-  hash = { content: content }
-  koment = post.comments.new(hash)
-  koment.user = User.second
-  koment.save
+# Comments
+puts 'creating random comments for each post ....'
+users = User.all
+Post.all.each do |post|
+  rand(3..30).times do
+    content =  Faker::Lorem.sentence(word_count: 30)
+    hash = { content: content }
+    koment = post.comments.new(hash)
+    koment.user = users.sample
+    koment.save
+  end
 end
